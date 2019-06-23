205577025d61dc20ec07b40cb8cfb577966a98c4
David Goffredo <dmgoffredo@gmail.com>
2019-06-23 16:22:37 -0400

    this one is too slow at the moment

---
+++ word-ladder/quadratic.py
@@ -0,0 +1,65 @@
+
+from typing import List
+
+
+class Solution:
+    def ladderLength(self,
+                     beginWord: str,
+                     endWord: str,
+                     wordList: List[str]) -> int:
+        return distance(beginWord, endWord, wordList)
+
+
+def without(string, i):
+    return string[:i] + string[i+1:]
+
+
+def neighbors_graph(words):
+    # graph :: word -> set(adjacent words...)
+    graph = {word: set() for word in words}
+
+    for word in words:
+        for other in words - set(word):
+            for i in range(len(word)):
+                if without(word, i) == without(other, i):
+                    graph[word].add(other)
+                    graph[other].add(word)
+
+    return graph
+
+
+def distance(begin, end, words):
+    """The plan is to build a graph of which words in `words + [begin]` are
+    connected to which other words. Then a depth first search through the graph
+    starting at `begin` will either find `end`, and thus the answer is the
+    current depth, or will not find `end`, meaning that there is no path, and
+    so the answer is zero.
+    """
+    words = set(words)
+    words.add(begin)
+
+    if end not in words:
+        return 0
+
+    graph = neighbors_graph(words)
+ 
+    nodes = [begin]
+    visited = set()
+    depth = 1
+    while nodes:
+        for node in nodes:
+            visited.add(node)
+
+        kids = set().union(*(graph[node] for node in nodes)) - visited
+        if not kids:
+            return 0
+
+        depth += 1
+
+        if end in kids:
+            return depth
+
+        nodes = kids
+
+    return depth
+    
