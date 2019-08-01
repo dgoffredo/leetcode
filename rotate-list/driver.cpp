cfb57633ea1acdd21a3a79530f71918bd63aa483
David Goffredo <dmgoffredo@gmail.com>
2019-08-01 10:18:34 -0400

    Why implement rotate() when you have the STL?

---
+++ rotate-list/driver.cpp
@@ -0,0 +1,4 @@
+#include "stl.cpp"
+
+int main() {
+}
---
+++ rotate-list/stl.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <iterator>
+#include <list>
+
+// leetcode.com boilerplate
+#ifdef WITH_LISTNODE
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+#endif
+
+std::list<int> node2list(const ListNode* head);
+
+ListNode* list2node(const std::list<int>& values);
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == nullptr) {
+            return head;
+        }
+
+        std::list<int> values = node2list(head);
+
+        // A rotation `k` to the right is the same as a rotation `n - k` to the
+        // left, where `n` is the length of the list.
+        const int n = values.size();
+
+        const auto newFirst = std::next(values.begin(), n - k % n);
+        std::rotate(values.begin(), newFirst, values.end());
+
+        return list2node(values);
+    }
+};
+
+std::list<int> node2list(const ListNode* head) {
+    std::list<int> values;
+
+    while (head) {
+        values.push_back(head->val);
+        head = head->next;
+    }
+
+    return values;
+}
+
+ListNode* list2node(const std::list<int>& values) {
+    if (values.empty()) {
+        return nullptr;
+    }
+
+    auto iter = values.begin();
+    const auto head = new ListNode(*iter);
+    ++iter;
+
+    for (auto current = head; iter != values.end(); ++iter) {
+        current = current->next = new ListNode(*iter);
+    }
+
+    return head;
+}
