7b2ca1300567805498e97fcb145aa65394636dcf
David Goffredo <dmgoffredo@gmail.com>
2019-08-07 16:15:56 -0400

    still wrong

--- basic-calculator/stacks.py
+++ basic-calculator/stacks.py
@@ -6,6 +6,7 @@ class Solution:
     def calculate(self, s: str) -> int:
         tokens = lex(s)
         polish = parse(tokens)
+        print('parsed:', polish)
         return evaluate(reversed(polish))
 
 
@@ -16,38 +17,39 @@ def lex(expression):
 def parse(tokens):
     args = []
     ops = []
-    depth = 0
+
+    def process_op(op):
+        print(op, 'just popped from', ops, 'and args are', args)
+        b, a = args.pop(), args.pop()
+        result = [op, a, b]
+        args.append(result)
+
     for token in tokens:
         try:
             token = int(token)
         except ValueError:
             pass
 
-        if token == '(':
-            depth += 1
+        if token == ')':
+            while True:
+                op = ops.pop()
+                if op == '(':
+                    break
+                process_op(op)
+        elif token == '(':
             ops.append(token)
-        elif token != ')':
+        else:
             if type(token) is int:
                 args.append(token)
             else:
                 ops.append(token)
-        else:
-            while True:
-                op = ops.pop()
-                if op == '(':
-                    depth -= 1
-                    break
 
-                b, a = args.pop(), args.pop()
-                result = [op, a, b]
-                args.append(result)
+            if len(args) > 1 and ops and ops[-1] != '(':
+                process_op(ops.pop())
 
     # process any remaining operations
     while ops:
-        op = ops.pop()
-        b, a = args.pop(), args.pop()
-        result = [op, a, b]
-        args.append(result)
+        process_op(ops.pop())
 
     print(args[0])
     return list(walk(args[0]))
@@ -66,12 +68,13 @@ def evaluate(instructions):
     stack = []
     for instruction in instructions:
         if instruction == '+':
-            b, a = stack.pop(), stack.pop()
+            a, b = stack.pop(), stack.pop()
             stack.append(a + b)
         elif instruction == '-':
-            b, a = stack.pop(), stack.pop()
+            a, b = stack.pop(), stack.pop()
             stack.append(a - b)
         else:
             stack.append(instruction)
+        print(stack)
 
     return stack[0]
