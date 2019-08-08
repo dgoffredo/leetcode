c101e52bd82f5bd906dff16c8c31a285829432a7
David Goffredo <dmgoffredo@gmail.com>
2019-08-08 10:54:23 -0400

    sweet Raptor Jesus, I finally got it

--- basic-calculator/stacks.py
+++ basic-calculator/stacks.py
@@ -5,76 +5,67 @@ import re
 class Solution:
     def calculate(self, s: str) -> int:
         tokens = lex(s)
-        polish = parse(tokens)
-        print('parsed:', polish)
-        return evaluate(reversed(polish))
+        reverse_polish = parse(tokens)
+        # print('parsed:', reverse_polish)
+        return evaluate(reverse_polish)
 
 
 def lex(expression):
     return re.findall(r'\(|\)|\+|-|[0-9]+', expression)
 
 
-def parse(tokens):
-    args = []
-    ops = []
+def maybe_parse_int(token):
+    try:
+        token = int(token)
+    except ValueError:
+        pass
 
-    def process_op(op):
-        print(op, 'just popped from', ops, 'and args are', args)
-        b, a = args.pop(), args.pop()
-        result = [op, a, b]
-        args.append(result)
+    return token
+
+
+def parse(tokens):
+    ops = [] # a stack
+    result = [] # a list
+
+    def process_ops():
+        while ops and ops[-1] != '(':
+            result.append(ops.pop())
 
     for token in tokens:
-        try:
-            token = int(token)
-        except ValueError:
-            pass
+        token = maybe_parse_int(token)
 
-        if token == ')':
-            while True:
-                op = ops.pop()
-                if op == '(':
-                    break
-                process_op(op)
-        elif token == '(':
-            ops.append(token)
+        # two cases:
+        #   - int -> push onto result.  If ops nonempty and last not "(", pop.
+        #   - str -> if not ")", push onto ops.  If ")", then assert ops ends
+        #            with "(".  Pop it and pop until next "(" or until empty.
+        if type(token) is int:
+            result.append(token)
+            process_ops()
         else:
-            if type(token) is int:
-                args.append(token)
-            else:
+            if token != ')':
                 ops.append(token)
+            else:
+                assert ops
+                assert ops[-1] == '('
+                ops.pop()
+                process_ops()
 
-            if len(args) > 1 and ops and ops[-1] != '(':
-                process_op(ops.pop())
-
-    # process any remaining operations
-    while ops:
-        process_op(ops.pop())
-
-    print(args[0])
-    return list(walk(args[0]))
-
-
-def walk(obj):
-    if type(obj) is not list:
-        yield obj
-        return
-
-    for item in obj:
-        yield from walk(item)
+    assert len(ops) == 0
+    return result
 
 
 def evaluate(instructions):
     stack = []
     for instruction in instructions:
         if instruction == '+':
-            a, b = stack.pop(), stack.pop()
+            b, a = stack.pop(), stack.pop()
             stack.append(a + b)
         elif instruction == '-':
-            a, b = stack.pop(), stack.pop()
+            b, a = stack.pop(), stack.pop()
             stack.append(a - b)
         else:
             stack.append(instruction)
-        print(stack)
+
+        # print(f'after instruction {instruction}: {stack}')
 
     return stack[0]
