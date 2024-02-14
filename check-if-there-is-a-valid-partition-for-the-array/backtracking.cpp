334637cc0d8bff851a30775d18652c095abcc58f
David Goffredo <dmgoffredo@gmail.com>
2024-02-14 00:09:24 -0500

    nope, too slow

--- check-if-there-is-a-valid-partition-for-the-array/backtracking.cpp
+++ check-if-there-is-a-valid-partition-for-the-array/backtracking.cpp
@@ -1,7 +1,21 @@
+// #define DEBUGGING
+
+#ifdef DEBUGGING
+#include <iostream>
+#define DEBUG for (bool XyZ = true; XyZ; std::cout << std::endl, XyZ = false) std::cout
+#else
+struct Ignore {};
+template <typename T>
+Ignore operator<<(const Ignore&, T&&) { return Ignore{}; }
+#define DEBUG if (false) Ignore{}
+#endif
+
 #include <cassert>
 #include <cstddef>
-#include <iostream>
 #include <vector>
+
+// for use by main() only...
+#include <iostream>
 
 using namespace std;
 
@@ -15,23 +29,25 @@ public:
 };
 
 bool is_valid_partition(const vector<int>& nums) {
-  // Each `int` in `pieces` is one of (1, 2, 3), corresponding to which way
-  // the piece of the partition matched from the problem statement.
-  // The special value zero means "haven't tried any of the cases yet."
-  // The width of each piece can be deduced from the match {1: 2, 2: 3, 3: 3}. 
-  vector<int> pieces;
+  struct Piece {
+    size_t index; // into `nums`
+    int criterion; // 1, 2, or 3; corresponding to the cases from the problem
+                   // or 0 for "haven't tried it yet."
+  };
+
+  vector<Piece> pieces;
   size_t i = 0; // offset into `nums`
-  pieces.push_back(0);
+  pieces.push_back(Piece{i, 0});
   while (i < nums.size()) {
-    std::cout << "nums.size(): " << nums.size() << "  i: " << i << "  pieces.size(): " << pieces.size() << "  pieces.back(): " << pieces.back() << std::endl;
-    switch (pieces.back()) {
+    DEBUG << "i: " << i << "  pieces.size(): " << pieces.size();
+    switch (pieces.back().criterion) {
     case 0: {
       // Try case 1: Subarray consists of two equal elements.
       if (i + 2 <= nums.size() && nums[i] == nums[i + 1]) {
-        std::cout << "    matched case 1" << std::endl;
-        pieces.back() = 1;
+        DEBUG << "    matched case 1";
+        pieces.back().criterion = 1;
         i += 2;
-        pieces.push_back(0);
+        pieces.push_back(Piece{i, 0});
         continue;
       }
     }
@@ -39,10 +55,10 @@ bool is_valid_partition(const vector<int>& nums) {
     case 1: {
       // Try case 2: Subarray consists of three equal elements.
       if (i + 3 <= nums.size() && nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2]) {
-        std::cout << "    matched case 2" << std::endl;
-        pieces.back() = 2;
+        DEBUG << "    matched case 2";
+        pieces.back().criterion = 2;
         i += 3;
-        pieces.push_back(0);
+        pieces.push_back(Piece{i, 0});
         continue;
       }
     }
@@ -50,56 +66,46 @@ bool is_valid_partition(const vector<int>& nums) {
     case 2: {
       // Try case 3: Subarray consists of a length three plus-one-each-time.
       if (i + 3 <= nums.size() && nums[i + 1] == nums[i] + 1 && nums[i + 2] == nums[i + 1] + 1) {
-        std::cout << "    matched case 3" << std::endl;
-        pieces.back() = 3;
+        DEBUG << "    matched case 2";
+        pieces.back().criterion = 3;
         i += 3;
-        pieces.push_back(0);
+        pieces.push_back(Piece{i, 0});
         continue;
       }
     }
-    } // switch (pieces.back())
+    } // switch (pieces.back().criterion)
   
+    DEBUG << "    didn't match any cases";
     // We've tried everything for the current position. Try to backtrack,
     // and try the next possible match in the previous piece.
-    pieces.pop_back();
-    if (pieces.empty()) {
-      // We've tried everything from the start. There's no match.
-      return false;
-    }
-
-    for (;;) {
-      std::cout << "    i: " << i << "   pieces.size(): " << pieces.size() << "   pieces.back(): " << pieces.back() << std::endl;
-      if (pieces.back() == 1) {
-        i -= 2;
-        ++pieces.back();
-        break;
-      } else if (pieces.back() == 2) {
-        i -= 3;
-        ++pieces.back();
-        break;
-      } else {
-        assert(pieces.back() == 3);
-        // Tried everything for this piece. Backtrack more.
-        i -= 3;
-        pieces.pop_back();
-        if (pieces.empty()) {
-          // We've tried everything from the start. There's no match.
-          return false;
-        }
+    do {
+      pieces.pop_back();
+      if (pieces.empty()) {
+        // We've tried everything from the start. There's no match.
+        return false;
       }
-    }
+    } while (pieces.back().criterion == 3);
+    i = pieces.back().index;
+    DEBUG << "    after rewind, i: " << i << "  pieces.size(): " << pieces.size() << "  pieces.back().criterion: " << pieces.back().criterion;
   } // while (i < nums.size())
   
+  assert(i == nums.size());
   // We found a match.
   return true;
 }
 
 // test
 int main() {
-  // std::cout << "Input: {4, 4, 4, 5, 6}" << std::endl;
+  // DEBUG << "Input: {4, 4, 4, 5, 6}";
   // return is_valid_partition({4, 4, 4, 5, 6});
-  // std::cout << "Input: {1, 1, 1, 2}" << std::endl;
+  // DEBUG << "Input: {1, 1, 1, 2}";
   // return is_valid_partition({1, 1, 1, 2});
-  std::cout << "Input: {1, 1, 1, 1, 2, 3}" << std::endl;
-  return is_valid_partition({1, 1, 1, 1, 2, 3});
+  // DEBUG << "Input: {1, 1, 1, 1, 2, 3}";
+  // return is_valid_partition({1, 1, 1, 1, 2, 3});
+  std::vector<int> input;
+  int value;
+  while (std::cin >> value) {
+    input.push_back(value);
+  }
+  return is_valid_partition(input);
 }
