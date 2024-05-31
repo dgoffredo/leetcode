dcb900ff6d003ce1ab7aa317102b21541a03de6a
David Goffredo <dmgoffredo@gmail.com>
2024-05-31 17:34:03 -0400

    still time limit exceeded...

--- maximum-alternating-subsequence-sum/memo.cpp
+++ maximum-alternating-subsequence-sum/memo.cpp
@@ -5,24 +5,24 @@
 template <typename Key, typename Value>
 using M = std::unordered_map<Key, Value>;
 
-// sum -> i -> sign -> answer
-using Cache = M<long long, M<int, M<int, int>>>;
+// i -> sign -> answer
+using Cache = M<int, M<int, long long>>;
 
-long long max_alternating_sum(Cache& cache, int sum, int i, int sign, const std::vector<int>& nums) {
+long long max_alternating_sum(Cache& cache, int i, int sign, const std::vector<int>& nums) {
   if (i == int(nums.size())) {
-    return sum;
+    return 0;
   }
 
-  auto& per_sign = cache[sum][i];
+  auto& per_sign = cache[i];
   const auto& entry = per_sign.find(sign);
   if (entry != per_sign.end()) {
     return entry->second;
   }
   return per_sign[sign] = std::max(
     // with `nums[i]`
-    max_alternating_sum(cache, sum + sign * nums[i], i + 1, -sign, nums),
+    sign * nums[i] + max_alternating_sum(cache, i + 1, -sign, nums),
     // without `nums[i]`
-    max_alternating_sum(cache, sum, i + 1, sign, nums)
+    max_alternating_sum(cache, i + 1, sign, nums)
   );
 }
 
@@ -30,6 +30,6 @@ class Solution {
 public:
     long long maxAlternatingSum(const std::vector<int>& nums) {
       Cache cache;
-      return max_alternating_sum(cache, 0, 0, +1, nums);
+      return max_alternating_sum(cache, 0, +1, nums);
     }
 };
