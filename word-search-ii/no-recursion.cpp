f96083466a8c8e91b56656a844e99d0d7097ac7e
David Goffredo <dmgoffredo@gmail.com>
2024-12-02 17:30:46 -0500

    just in case

---
+++ word-search-ii/no-recursion.cpp
@@ -0,0 +1,155 @@
+#include <array>
+#include <cctype>
+#include <memory>
+#include <string>
+#include <vector>
+
+// We can mark a grid cell as "visited" by making it upper case, as an
+// optimization. Then unmark it by making it lower case again.
+
+bool is_lower(char letter) {
+    return letter >= 'a' && letter <= 'z';
+}
+
+// The need for these wrappers is one of the dumbest things about C++.
+
+char to_upper(unsigned char letter) {
+    return std::toupper(letter);
+}
+
+char to_lower(unsigned char letter) {
+    return std::tolower(letter);
+}
+
+struct PrefixTrieNode {
+    std::string *match = nullptr;
+    std::array<PrefixTrieNode*, 26> children = {};
+
+    PrefixTrieNode*& operator[](char letter) {
+        return children[letter - 'a'];
+    }
+};
+
+struct PrefixTrieDeleter {
+    void operator()(PrefixTrieNode *node) const {
+        if (node == nullptr) {
+            return;
+        }
+
+        std::vector<PrefixTrieNode*> stack = {node};
+        do {
+            auto node = stack.back();
+            stack.pop_back();    
+            for (auto *child : node->children) {
+                if (child) {
+                    stack.push_back(child);
+                }
+            }
+            delete node;
+        } while (!stack.empty());
+    }
+};
+
+using PrefixTrie = std::unique_ptr<PrefixTrieNode, PrefixTrieDeleter>;
+
+void add_word_to_trie(PrefixTrieNode& trie, std::string& word) {
+    PrefixTrieNode *node = &trie;
+    for (const char letter : word) {
+        PrefixTrieNode*& child = (*node)[letter];
+        if (child == nullptr) {
+            child = new PrefixTrieNode();
+        }
+        node = child;
+    }
+    if (node->match == nullptr) {
+        node->match = &word;
+    }
+}
+
+PrefixTrie build_prefix_trie(std::vector<std::string>& words) {
+    auto trie = PrefixTrie(new PrefixTrieNode());
+    for (std::string& word : words) {
+        add_word_to_trie(*trie, word);
+    }
+    return trie;
+}
+
+std::vector<std::string> find_words(std::vector<std::vector<char>>& board, std::vector<std::string>& words) {
+    std::vector<std::string> found;
+    PrefixTrie trie = build_prefix_trie(words);
+    const int rows = board.size();
+    const int cols = board[0].size();
+
+    struct Frame {
+        int r;
+        int c;
+        PrefixTrieNode *node;
+    };
+
+    std::vector<Frame> stack;
+
+    // const auto visit = [&](int r, int c, PrefixTrieNode* node, auto& recur) {
+    const auto consume_stack = [&]() {
+        do {
+            auto [r, c, node] = stack.back();
+
+            if (!is_lower(board[r][c])) {
+                // Unmark this square.
+                board[r][c] = to_lower(board[r][c]);
+                stack.pop_back();
+                continue;
+            }
+
+            node = (*node)[board[r][c]];
+            if (node == nullptr) {
+                // We're no longer a prefix of some word.
+                stack.pop_back();
+                continue;
+            }
+            if (node->match) {
+                // We found a word.
+                found.push_back(std::move(*node->match));
+                // Remove the match from the trie so we don't "find" it again.
+                node->match = nullptr;
+            }
+
+            // Mark this square as "visited."
+            board[r][c] = to_upper(board[r][c]);
+            
+            // Examine eligible neighbors.
+            // above:
+            if (r > 0 && is_lower(board[r - 1][c])) {
+                stack.push_back(Frame{r - 1, c, node});
+            }
+            // below:
+            if (r < rows - 1 && is_lower(board[r + 1][c])) {
+                stack.push_back(Frame{r + 1, c, node});
+            }
+            // leftward:
+            if (c > 0 && is_lower(board[r][c - 1])) {
+                stack.push_back(Frame{r, c - 1, node});
+            }
+            // rightward:
+            if (c < cols - 1 && is_lower(board[r][c + 1])) {
+                stack.push_back(Frame{r, c + 1, node});
+            }
+
+        } while (!stack.empty());
+    };
+
+    for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
+            // (r, c) is the starting point. Snake around the grid looking for words.
+            stack.push_back(Frame{r, c, trie.get()});
+            consume_stack();
+        }
+    }
+    return found;
+}
+
+class Solution {
+public:
+    std::vector<std::string> findWords(std::vector<std::vector<char>>& board, std::vector<std::string>& words) {
+        return find_words(board, words);
+    }
+};
